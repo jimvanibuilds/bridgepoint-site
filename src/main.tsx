c20a35c7caa730b02dd0c0a88a0775e5888d70f9
jimvanibuilds <prestigegroupproperties@gmail.com>
2025-10-15 15:12:27 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Index from "./pages/Index"; // or "./pages/index" if lowercase in your repo
+import Index from "./pages/index"; // or "./pages/index" if lowercase in your repo
 import "./index.css"; // safe to keep even if you don’t have it
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
